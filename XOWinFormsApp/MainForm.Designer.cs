8d93e14ecf91aca47e2829ad880244481b171d1b
Petru4e <Petru4e85@mail.ru>
2023-02-13 19:26:44 +0300

    Добавьте файлы проекта.

---
+++ XOWinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace XOWinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1button = new System.Windows.Forms.Button();
+            this.a2button = new System.Windows.Forms.Button();
+            this.a3button = new System.Windows.Forms.Button();
+            this.b3button = new System.Windows.Forms.Button();
+            this.b2button = new System.Windows.Forms.Button();
+            this.b1button = new System.Windows.Forms.Button();
+            this.c3button = new System.Windows.Forms.Button();
+            this.c2button = new System.Windows.Forms.Button();
+            this.c1button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.xWinCountLabel = new System.Windows.Forms.Label();
+            this.drawCountLabel = new System.Windows.Forms.Label();
+            this.oWinCountLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.фаилToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рестартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.скинутьСтатистикуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // a1button
+            // 
+            this.a1button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a1button.Location = new System.Drawing.Point(16, 56);
+            this.a1button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.a1button.Name = "a1button";
+            this.a1button.Size = new System.Drawing.Size(100, 100);
+            this.a1button.TabIndex = 0;
+            this.a1button.UseVisualStyleBackColor = true;
+            this.a1button.Click += new System.EventHandler(this.a1button_Click);
+            this.a1button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.a1button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // a2button
+            // 
+            this.a2button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a2button.Location = new System.Drawing.Point(120, 57);
+            this.a2button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.a2button.Name = "a2button";
+            this.a2button.Size = new System.Drawing.Size(100, 100);
+            this.a2button.TabIndex = 1;
+            this.a2button.UseVisualStyleBackColor = true;
+            this.a2button.Click += new System.EventHandler(this.a1button_Click);
+            this.a2button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.a2button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // a3button
+            // 
+            this.a3button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a3button.Location = new System.Drawing.Point(224, 57);
+            this.a3button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.a3button.Name = "a3button";
+            this.a3button.Size = new System.Drawing.Size(100, 100);
+            this.a3button.TabIndex = 2;
+            this.a3button.UseVisualStyleBackColor = true;
+            this.a3button.Click += new System.EventHandler(this.a1button_Click);
+            this.a3button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.a3button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // b3button
+            // 
+            this.b3button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b3button.Location = new System.Drawing.Point(224, 160);
+            this.b3button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b3button.Name = "b3button";
+            this.b3button.Size = new System.Drawing.Size(100, 100);
+            this.b3button.TabIndex = 5;
+            this.b3button.UseVisualStyleBackColor = true;
+            this.b3button.Click += new System.EventHandler(this.a1button_Click);
+            this.b3button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.b3button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // b2button
+            // 
+            this.b2button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b2button.Location = new System.Drawing.Point(120, 161);
+            this.b2button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b2button.Name = "b2button";
+            this.b2button.Size = new System.Drawing.Size(100, 100);
+            this.b2button.TabIndex = 4;
+            this.b2button.UseVisualStyleBackColor = true;
+            this.b2button.Click += new System.EventHandler(this.a1button_Click);
+            this.b2button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.b2button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // b1button
+            // 
+            this.b1button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b1button.Location = new System.Drawing.Point(16, 160);
+            this.b1button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b1button.Name = "b1button";
+            this.b1button.Size = new System.Drawing.Size(100, 100);
+            this.b1button.TabIndex = 3;
+            this.b1button.UseVisualStyleBackColor = true;
+            this.b1button.Click += new System.EventHandler(this.a1button_Click);
+            this.b1button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.b1button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // c3button
+            // 
+            this.c3button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.c3button.Location = new System.Drawing.Point(224, 264);
+            this.c3button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.c3button.Name = "c3button";
+            this.c3button.Size = new System.Drawing.Size(100, 100);
+            this.c3button.TabIndex = 8;
+            this.c3button.UseVisualStyleBackColor = true;
+            this.c3button.Click += new System.EventHandler(this.a1button_Click);
+            this.c3button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.c3button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // c2button
+            // 
+            this.c2button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.c2button.Location = new System.Drawing.Point(120, 264);
+            this.c2button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.c2button.Name = "c2button";
+            this.c2button.Size = new System.Drawing.Size(100, 100);
+            this.c2button.TabIndex = 7;
+            this.c2button.UseVisualStyleBackColor = true;
+            this.c2button.Click += new System.EventHandler(this.a1button_Click);
+            this.c2button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.c2button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // c1button
+            // 
+            this.c1button.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.c1button.Location = new System.Drawing.Point(16, 264);
+            this.c1button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.c1button.Name = "c1button";
+            this.c1button.Size = new System.Drawing.Size(100, 100);
+            this.c1button.TabIndex = 6;
+            this.c1button.UseVisualStyleBackColor = true;
+            this.c1button.Click += new System.EventHandler(this.a1button_Click);
+            this.c1button.MouseEnter += new System.EventHandler(this.a1button_MouseEnter);
+            this.c1button.MouseLeave += new System.EventHandler(this.a1button_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 386);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Выиграно X: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(117, 386);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ничья: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(221, 386);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Выиграно O: ";
+            // 
+            // xWinCountLabel
+            // 
+            this.xWinCountLabel.AutoSize = true;
+            this.xWinCountLabel.Location = new System.Drawing.Point(20, 426);
+            this.xWinCountLabel.Name = "xWinCountLabel";
+            this.xWinCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.xWinCountLabel.TabIndex = 12;
+            this.xWinCountLabel.Text = "0";
+            // 
+            // drawCountLabel
+            // 
+            this.drawCountLabel.AutoSize = true;
+            this.drawCountLabel.Location = new System.Drawing.Point(117, 426);
+            this.drawCountLabel.Name = "drawCountLabel";
+            this.drawCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.drawCountLabel.TabIndex = 13;
+            this.drawCountLabel.Text = "0";
+            // 
+            // oWinCountLabel
+            // 
+            this.oWinCountLabel.AutoSize = true;
+            this.oWinCountLabel.Location = new System.Drawing.Point(221, 426);
+            this.oWinCountLabel.Name = "oWinCountLabel";
+            this.oWinCountLabel.Size = new System.Drawing.Size(13, 13);
+            this.oWinCountLabel.TabIndex = 14;
+            this.oWinCountLabel.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фаилToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(343, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // фаилToolStripMenuItem
+            // 
+            this.фаилToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рестартToolStripMenuItem,
+            this.скинутьСтатистикуToolStripMenuItem});
+            this.фаилToolStripMenuItem.Name = "фаилToolStripMenuItem";
+            this.фаилToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.фаилToolStripMenuItem.Text = "Фаил";
+            // 
+            // рестартToolStripMenuItem
+            // 
+            this.рестартToolStripMenuItem.Name = "рестартToolStripMenuItem";
+            this.рестартToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.рестартToolStripMenuItem.Text = "Рестарт";
+            this.рестартToolStripMenuItem.Click += new System.EventHandler(this.рестартToolStripMenuItem_Click);
+            // 
+            // скинутьСтатистикуToolStripMenuItem
+            // 
+            this.скинутьСтатистикуToolStripMenuItem.Name = "скинутьСтатистикуToolStripMenuItem";
+            this.скинутьСтатистикуToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.скинутьСтатистикуToolStripMenuItem.Text = "Скинуть статистику";
+            this.скинутьСтатистикуToolStripMenuItem.Click += new System.EventHandler(this.скинутьСтатистикуToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 464);
+            this.Controls.Add(this.oWinCountLabel);
+            this.Controls.Add(this.drawCountLabel);
+            this.Controls.Add(this.xWinCountLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.c3button);
+            this.Controls.Add(this.c2button);
+            this.Controls.Add(this.c1button);
+            this.Controls.Add(this.b3button);
+            this.Controls.Add(this.b2button);
+            this.Controls.Add(this.b1button);
+            this.Controls.Add(this.a3button);
+            this.Controls.Add(this.a2button);
+            this.Controls.Add(this.a1button);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "КрестикиНолики";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1button;
+        private System.Windows.Forms.Button a2button;
+        private System.Windows.Forms.Button a3button;
+        private System.Windows.Forms.Button b3button;
+        private System.Windows.Forms.Button b2button;
+        private System.Windows.Forms.Button b1button;
+        private System.Windows.Forms.Button c3button;
+        private System.Windows.Forms.Button c2button;
+        private System.Windows.Forms.Button c1button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label xWinCountLabel;
+        private System.Windows.Forms.Label drawCountLabel;
+        private System.Windows.Forms.Label oWinCountLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem фаилToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рестартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem скинутьСтатистикуToolStripMenuItem;
+    }
+}
+
---
+++ XOWinFormsApp/MainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOWinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        bool flag = true; // true = X, false = O
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void a1button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            if(flag==true)
+            {
+                clickedButton.Text = "X";
+            }
+            else
+            {
+                clickedButton.Text = "O";
+            }
+            clickedButton.Enabled = false;
+            CheckWin();
+            flag = !flag;
+        }
+        void CheckWin()
+        {
+            string winner = flag == true ? "X" : "O";
+
+            //по горизонтали
+            if(a1button.Text!="" && a1button.Text==a2button.Text && a3button.Text==a1button.Text)
+            {
+                MessageBox.Show("Выиграл "+ winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+            if (b1button.Text != "" && b1button.Text == b2button.Text && b3button.Text == b1button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            } 
+            if (c1button.Text != "" && c1button.Text == c2button.Text && c3button.Text == c1button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+
+            //по вертикали
+            if(a1button.Text != "" && a1button.Text == b1button.Text && c1button.Text == a1button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+            if (a2button.Text != "" && a2button.Text == b2button.Text && c2button.Text == a2button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+            if (a3button.Text != "" && a3button.Text == b3button.Text && c3button.Text == a3button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+
+            //  диагонали
+
+            if (a1button.Text != "" && a1button.Text == b2button.Text && c3button.Text == a1button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+            if (a3button.Text != "" && a3button.Text == b2button.Text && c1button.Text == a3button.Text)
+            {
+                MessageBox.Show("Выиграл " + winner);
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+            if(a1button.Enabled==false && a2button.Enabled==false && a3button.Enabled==false &&
+                b1button.Enabled == false && b2button.Enabled == false && b3button.Enabled == false&&
+                    c1button.Enabled == false && c2button.Enabled == false && c3button.Enabled == false)
+            {
+                MessageBox.Show("Ничья");
+                winner = "d";
+                IncreaseWinCount(winner);
+                Restart();
+                return;
+            }
+        }
+
+        private void IncreaseWinCount(string result)
+        {
+            if (result=="d")
+            {
+                drawCountLabel.Text = (Convert.ToInt32(drawCountLabel.Text) + 1).ToString();
+            }
+            else
+            if (result=="X")
+            {
+                xWinCountLabel.Text = (Convert.ToInt32(xWinCountLabel.Text) + 1).ToString();
+            }
+            else
+            {
+                oWinCountLabel.Text = (Convert.ToInt32(oWinCountLabel.Text) + 1).ToString();
+            }
+        }
+
+        void Restart()
+        {
+            foreach (var control in Controls)
+            {
+                if (control is Button)
+                {
+                    Button button = (Button)control;
+                    button.Text = "";
+                    button.Enabled = true;
+                }
+            }
+        }
+        private void рестартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Restart();
+            //foreach (var control in Controls)
+            //{
+            //    if(control is Button)
+            //    {
+            //        Button button = (Button)control;
+            //        button.Text = "";
+            //        button.Enabled = true;
+            //    }
+            //}
+        }
+
+        private void скинутьСтатистикуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            xWinCountLabel.Text = "0";
+            oWinCountLabel.Text = "0";
+            drawCountLabel.Text = "0";
+        }
+
+        private void a1button_MouseEnter(object sender, EventArgs e)
+        {
+            Button enteredButton = (Button)sender;
+            if (flag == true)
+            {
+                enteredButton.Text = "X";
+            }
+            else
+            {
+                enteredButton.Text = "O";
+            }
+
+        }
+
+        private void a1button_MouseLeave(object sender, EventArgs e)
+        {
+            Button leavedButton = (Button)sender;
+            if(leavedButton.Enabled==true)
+            {
+                leavedButton.Text = "";
+            }
+        }
+    }
+}
